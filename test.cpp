6fa6b8d66268a8dc64778135570c249656375e08
rdavilao <rdavilao@outlook.com>
2025-04-25 09:31:53 -0500

    Standard version

--- test.cpp
+++ test.cpp
@@ -2,15 +2,7 @@
 
 using namespace std;
 
-void saludar();
-
 int main() {
 
-    cout << "Hola" << endl;
-    cout << "Github" << endl;
     return 0;
-}
-
-void saludar() {
-    cout << "Hola" << endl;
 }
