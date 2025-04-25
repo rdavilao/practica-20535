73e9f8e9e6b33ba04cf0584c94555bbd3449b84d
rdavilao <rdavilao@outlook.com>
2025-04-25 09:13:59 -0500

    Add saludar function

--- test.cpp
+++ test.cpp
@@ -2,9 +2,15 @@
 
 using namespace std;
 
+void saludar();
+
 int main() {
 
     cout << "Hola" << endl;
     cout << "Github" << endl;
     return 0;
+}
+
+void saludar() {
+    cout << "Hola" << endl;
 }
